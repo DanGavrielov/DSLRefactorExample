c6007b881cdcc7987ee9509fa3ddd430f09eb2ca
dang <dang@gini-apps.com>
2022-07-27 17:09:30 +0300

    added presentation

---
+++ src/main/kotlin/presentation/Util.kt
@@ -0,0 +1,3 @@
+package presentation
+
+class Person
---
+++ src/main/kotlin/presentation/java_utils/PlatformTypes.java
@@ -0,0 +1,17 @@
+package presentation.java_utils;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlatformTypes {
+    public static List<Integer> getJavaList() {
+        return new ArrayList<>();
+    }
+
+    public static List<Integer> getNullList() {
+        return null;
+    }
+}
